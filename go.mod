f1ede34849e7172427c84411f61e46d0d8a782c0
Mauro Lavigna <mauro.lavigna@gmail.com>
2021-04-28 18:12:38 -0300

    try testify/assert out for tests

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/regul4rj0hn/bookstore-oauth-api
 
 go 1.16
+
+require github.com/stretchr/testify v1.7.0
--- pkg/domain/token/token_test.go
+++ pkg/domain/token/token_test.go
@@ -3,37 +3,25 @@ package access_token
 import (
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAccessTokenConstants(t *testing.T) {
-	if expirationTime != 24 {
-		t.Error("default token expiration time should be 24 hours")
-	}
+	assert.EqualValues(t, 24, expirationTime, "default token expiration time should be 24 hours")
 }
 
 func TestGetAccessToken(t *testing.T) {
 	at := GetAccessToken()
-	if at.IsExpired() {
-		t.Error("new access token should not be expired")
-	}
-
-	if at.Token != "" {
-		t.Error("new access token should not have valid id")
-	}
-
-	if at.Subject != 0 {
-		t.Error("new access token should not have a subject associated")
-	}
+	assert.False(t, at.IsExpired(), "new access token should not be expired")
+	assert.EqualValues(t, "", at.Token, "new access token should not have valid id")
+	assert.True(t, at.Subject == 0, "new access token should not have a subject associated")
 }
 
 func TestIsExpired(t *testing.T) {
 	at := AccessToken{}
-	if !at.IsExpired() {
-		t.Error("empty access token should be expired by default")
-	}
+	assert.True(t, at.IsExpired(), "empty access token should be expired by default")
 
 	at.Expires = time.Now().UTC().Add(3 * time.Hour).Unix()
-	if at.IsExpired() {
-		t.Error("access token expiring three hours from now should not be expired")
-	}
+	assert.False(t, at.IsExpired(), "access token expiring three hours from now should not be expired")
 }
