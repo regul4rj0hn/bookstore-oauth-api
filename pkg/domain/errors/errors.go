40dfc397bb23691ed0d15cf8e1252d8872edc6de
Mauro Lavigna <mauro.lavigna@gmail.com>
2021-05-08 21:40:18 -0300

    clean up errors model

--- pkg/domain/errors/errors.go
+++ pkg/domain/errors/errors.go
@@ -1,10 +1,16 @@
 package errors
 
 import (
-	"errors"
 	"net/http"
 
 	"github.com/go-chi/render"
+)
+
+var (
+	ErrBadRequest     = &Response{StatusCode: http.StatusBadRequest, StatusText: "bad_request"}
+	ErrNotFound       = &Response{StatusCode: http.StatusNotFound, StatusText: "not_found"}
+	ErrConflict       = &Response{StatusCode: http.StatusConflict, StatusText: "conflict"}
+	ErrInternalServer = &Response{StatusCode: http.StatusInternalServerError, StatusText: "internal_server_error"}
 )
 
 type Response struct {
@@ -13,26 +19,25 @@ type Response struct {
 	Message    string `json:"message"`
 }
 
-func (e *Response) Render(w http.ResponseWriter, r *http.Request) error {
-	render.Status(r, e.StatusCode)
-	return nil
-}
-
-func Message(msg string) error {
-	return errors.New(msg)
-}
-
 func BadRequest(msg string) *Response {
 	return &Response{
-		StatusText: "bad_request",
+		StatusText: ErrBadRequest.StatusText,
 		StatusCode: http.StatusBadRequest,
+		Message:    msg,
+	}
+}
+
+func NotFound(msg string) *Response {
+	return &Response{
+		StatusText: ErrNotFound.StatusText,
+		StatusCode: http.StatusNotFound,
 		Message:    msg,
 	}
 }
 
 func Conflict(msg string) *Response {
 	return &Response{
-		StatusText: "conflict",
+		StatusText: ErrConflict.StatusText,
 		StatusCode: http.StatusConflict,
 		Message:    msg,
 	}
@@ -40,16 +45,13 @@ func Conflict(msg string) *Response {
 
 func InternalServerError(msg string) *Response {
 	return &Response{
-		StatusText: "internal_server_error",
+		StatusText: ErrInternalServer.StatusText,
 		StatusCode: http.StatusInternalServerError,
 		Message:    msg,
 	}
 }
 
-func NotFound(msg string) *Response {
-	return &Response{
-		StatusText: "not_found",
-		StatusCode: http.StatusNotFound,
-		Message:    msg,
-	}
+func (e *Response) Render(w http.ResponseWriter, r *http.Request) error {
+	render.Status(r, e.StatusCode)
+	return nil
 }
