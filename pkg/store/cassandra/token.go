1c9cee1aebb40816e5672046a5403d1076695e43
Mauro Lavigna <mauro.lavigna@gmail.com>
2021-05-10 20:07:18 -0300

    implement get token by id

--- pkg/store/cassandra/token.go
+++ pkg/store/cassandra/token.go
@@ -1,9 +1,14 @@
 package cassandra
 
 import (
+	"github.com/gocql/gocql"
 	database "github.com/regul4rj0hn/bookstore-oauth-api/pkg/clients/cassandra"
 	"github.com/regul4rj0hn/bookstore-oauth-api/pkg/domain/errors"
 	"github.com/regul4rj0hn/bookstore-oauth-api/pkg/domain/token"
+)
+
+const (
+	queryGetAccessToken = "SELECT access_token, client_id, sub, expires FROM tokens WHERE access_token=?;"
 )
 
 type TokenStore struct{}
@@ -12,12 +17,20 @@ func NewTokenStore() *TokenStore {
 	return &TokenStore{}
 }
 
-func (ts *TokenStore) GetById(string) (*token.AccessToken, *errors.Response) {
+func (ts *TokenStore) GetById(id string) (*token.AccessToken, *errors.Response) {
 	session, err := database.GetSession()
 	if err != nil {
-		panic(err)
+		return nil, errors.InternalServerError(err.Error())
 	}
 	defer session.Close()
 
-	return nil, errors.InternalServerError("database connection not implemented")
+	var token token.AccessToken
+	if err := session.Query(queryGetAccessToken, id).Scan(&token.AccessToken, &token.ClientId, &token.Subject, &token.Expires); err != nil {
+		if err == gocql.ErrNotFound {
+			return nil, errors.NotFound("token not found")
+		}
+		return nil, errors.InternalServerError(err.Error())
+	}
+
+	return &token, nil
 }
